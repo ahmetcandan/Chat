f642e70e11789d1066be1cdfeaca2734392e6932
ahmetcandan <candanahm@gmail.com>
2021-06-12 15:06:10 +0300

    Encrypt, Decrypt methodları iyileştirildi

--- Chat.Core/Cryptography/RSA.cs
+++ Chat.Core/Cryptography/RSA.cs
@@ -9,42 +9,18 @@ namespace Chat.Core.Cryptography
 {
     public static class RSA
     {
-        private static UnicodeEncoding _encoder = new UnicodeEncoding();
-
         public static string Decrypt(this string data, string privateKey)
         {
             var rsa = new RSACryptoServiceProvider();
-            var dataArray = data.Split(new char[] { ',' });
-            byte[] dataByte = new byte[dataArray.Length];
-            for (int i = 0; i < dataArray.Length; i++)
-            {
-                dataByte[i] = Convert.ToByte(dataArray[i]);
-            }
-
             rsa.FromXmlString(privateKey);
-            var decryptedByte = rsa.Decrypt(dataByte, false);
-            return _encoder.GetString(decryptedByte);
+            return Encoding.UTF8.GetString(rsa.Decrypt(data.Split(',').Select(c => Convert.ToByte(c)).ToArray(), false));
         }
 
         public static string Encrypt(this string data, string publicKey)
         {
             var rsa = new RSACryptoServiceProvider();
             rsa.FromXmlString(publicKey);
-            var dataToEncrypt = _encoder.GetBytes(data);
-            var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
-            var length = encryptedByteArray.Count();
-            var item = 0;
-            var sb = new StringBuilder();
-            foreach (var x in encryptedByteArray)
-            {
-                item++;
-                sb.Append(x);
-
-                if (item < length)
-                    sb.Append(",");
-            }
-
-            return sb.ToString();
+            return string.Join(",", rsa.Encrypt(Encoding.UTF8.GetBytes(data), false).ToArray());
         }
 
         public static byte[] _Decrypt(this byte[] data, string privateKey)
@@ -53,7 +29,7 @@ namespace Chat.Core.Cryptography
             rsa.FromXmlString(privateKey);
             return rsa.Decrypt(data, false);
         }
-        
+
         public static byte[] _Encrypt(this byte[] data, string publicKey)
         {
             var rsa = new RSACryptoServiceProvider();
@@ -63,12 +39,12 @@ namespace Chat.Core.Cryptography
 
         public static byte[] ByteArray(this string data)
         {
-            return _encoder.GetBytes(data);
+            return Encoding.UTF8.GetBytes(data);
         }
 
         public static string ByteArrayToString(this byte[] data)
         {
-            return _encoder.GetString(data);
+            return Encoding.UTF8.GetString(data);
         }
     }
 }
